4b261690a891d688cc5e149d06e7766329e2bb76
Gabrielle Rife <43075585+gaberife@users.noreply.github.com>
2019-01-21 13:41:17 -0500

    Update Polynomial.h

--- Polynomial.h
+++ Polynomial.h
@@ -1,3 +1,4 @@
+
 #ifndef POLYNOMIAL_H
 #define POLYNOMIAL_H
 #include <iostream>
@@ -7,21 +8,21 @@ using namespace std;
 
 class Polynomial {
 
+ private:
+  int degree;
+  double* coeff[];
+
  public:
-  Polynomial(int degree, double* coeff[]);
+  Polynomial (int degree. double* coeff[]): degree(getDegree), coeff(getCoeff){}
   double evalAt(double coeff[], int degree, double eval);
   void toString(double coeff[], int degree, string &result);
   bool findZeros(double coeff[], int degree, double out[]);
   const int getDegree();
   const double getCoeff(int degree);
   void setCoeff(int Deg, double newCoeff);
- Polynomial operator == (const Polynomial & other);
- Polynomial operator != (const Polynomial & other);
-
- private:
-  int degree;
-  double* coeff[];
-
+  Polynomial operator == (const Polynomial & other);
+  Polynomial operator != (const Polynomial & other);
 };
 
 #endif
+
